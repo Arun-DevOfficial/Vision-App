1510cf290ec6eeccfb0b514bf5222ab9527bcf41
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-09-17 09:36:07 +0530

    Update html

--- Client/src/Pages/Blog.Editor.jsx
+++ Client/src/Pages/Blog.Editor.jsx
@@ -97,58 +97,55 @@ export default function BlogEditor() {
 
   return (
     <>
-      <nav className="p-5 flex justify-between font-poppins">
-        <div className="flex items-center gap-4">
+      <section className="container mx-auto">
+        <nav className="p-5 flex justify-between font-poppins">
           <a href="/" className="font-bold text-3xl cursor-pointer font-title">
             Vision
           </a>
-          <p className="font-medium max-md:hidden text-black line-clamp-1 w-full">
-            New Blog
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button
-            onClick={handlePublishEvent}
-            className="bg-black rounded-full font-medium text-white px-5 py-2"
-          >
-            Publish
-          </button>
-          <button className="bg-[#ededed] font-medium rounded-full px-5 py-2">
-            Save Draft
-          </button>
-        </div>
-      </nav>
-      <section>
-        <div className="w-full max-w-[900px] mx-auto p-4">
-          <div className="relative aspect-video hover:opacity-80 border-4 bg-white border-grey">
-            <label htmlFor="uploadBanner">
-              <img
-                src={banner || DefaultBanner}
-                alt="Blog"
-                onError={handleBlogBannerError}
-                className="z-20 bg-cover cursor-pointer bg-center"
-              />
-              <input
-                type="file"
-                id="uploadBanner"
-                accept=".png, .jpg, .jpeg"
-                hidden
-                onChange={handleBannerUpload}
-              />
-            </label>
+          <div className="flex gap-3">
+            <button
+              onClick={handlePublishEvent}
+              className="bg-black rounded-full font-medium text-white px-5 py-2"
+            >
+              Publish
+            </button>
+            <button className="bg-[#ededed] font-medium rounded-full px-5 py-2">
+              Save Draft
+            </button>
           </div>
-          <textarea
-            placeholder="Blog Title"
-            className="text-4xl font-bold w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40 font-poppins"
-            onKeyDown={handleTitleKeyDown}
-            onChange={handleTitleChange}
-            value={title}
-          />
-          <hr className="w-full opacity-40 mb-5" />
-          <div id="text-Editor" className="font-gelasio"></div>
-        </div>
+        </nav>
+        <section>
+          <div className="w-full max-w-[900px] mx-auto p-4">
+            <div className="relative aspect-video hover:opacity-80 border-4 bg-white border-grey">
+              <label htmlFor="uploadBanner">
+                <img
+                  src={banner || DefaultBanner}
+                  alt="Blog"
+                  onError={handleBlogBannerError}
+                  className="z-20 bg-cover cursor-pointer bg-center"
+                />
+                <input
+                  type="file"
+                  id="uploadBanner"
+                  accept=".png, .jpg, .jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
+            <textarea
+              placeholder="Blog Title"
+              className="text-4xl font-bold w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40 font-poppins"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+              value={title}
+            />
+            <hr className="w-full opacity-40 mb-5" />
+            <div id="text-Editor" className="font-gelasio"></div>
+          </div>
+        </section>
+        <Toaster />
       </section>
-      <Toaster />
     </>
   );
 }
