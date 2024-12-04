83e90169fc6d0cc46ca8871882bfa621592b2743
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-12-04 16:23:17 +0530

    updated

--- Server/Controllers/auth.js
+++ Server/Controllers/auth.js
@@ -60,7 +60,7 @@ export const handleSignIn = async (req, res) => {
     if (!isMatch) {
       return res
         .status(400)
-        .json({ ErrorMessage: "Invalid email or password!" });
+        .json({ ErrorMessage: "Invalid credentials!" });
     }
     // Payload for the JWT
     const payload = {
@@ -74,7 +74,7 @@ export const handleSignIn = async (req, res) => {
     // Set the token in a cookie
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: strict, // Updated to "strict" for better security
+      sameSite: true, // Updated to "strict" for better security
       Secure: false, // Secure should be true in production
     });
     // Respond with success
