2a472a2d056c3275186457631743e149b56d4200
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-12-06 10:40:07 +0530

    updated

--- Server/Config/DB.js
+++ Server/Config/DB.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://arunkum:dev_2050@cluster0.jaxeuud.mongodb.net/Restaurant_App?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://arunkum:QYj2C4hMS2tOWjic@cluster0.jaxeuud.mongodb.net/Restaurant_App?retryWrites=true&w=majority&appName=Cluster0");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
     process.exit(1); // Exit the process with a failure code
